98f2bec377018430962af4b0618b934a71e07b73
Mahsa Shiri <mhsshr02@gmail.com>
2025-09-07 09:33:16 +0330

    add a solution

---
+++ Way Too Long Words/wayTooLongWords.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+   int n;
+   cin >> n;
+   string word;
+   for(int i = 0; i < n; i ++){
+    cin >> word;
+    if(word.size() <= 10){
+        cout << word;
+    }
+    else{
+        cout << word[0] << word.size() - 2 << word[word.size() - 1];
+    }
+    cout << "\n";
+   }  
+}
